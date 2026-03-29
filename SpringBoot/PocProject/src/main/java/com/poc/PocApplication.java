2636a1cccccea70c7bc75d520b1d14a91ba36cdb
kamalakshancg <kamalaksha.ncg@gmail.com>
2026-03-29 19:26:26 +0530

    Refactored

--- SpringBoot/PocProject/src/main/java/com/poc/PocApplication.java
+++ SpringBoot/PocProject/src/main/java/com/poc/PocApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PocProjectApplication {
-
+public class PocApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(PocProjectApplication.class, args);
+		SpringApplication.run(PocApplication.class, args);
 	}
-
 }
